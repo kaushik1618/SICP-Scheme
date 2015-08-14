6a3a9bd50e4acbc04846e5de303bf26ca31d202a
Kaushik Kannan <kaushik1618@gmail.com>
2015-08-13 19:31:18 -0700

    Derivative of a polynomial- simplified expression

--- deriv-poly.lisp
+++ deriv-poly.lisp
@@ -14,9 +14,21 @@
 
 (define (product? exp) (and (not (atom? exp)) (eq? (car exp) '*)))
 
-(define (make-sum a b) (list '+ a b))
+(define (make-sum a b)
+  (cond ((and (integer? a) (= a 0)) b)
+	((and (integer? b) (= b 0)) a)
+	((and (integer? a) (integer? b)) (+ a b))
+	((eq? a b) (list '* 2 a))
+	(else (list '+ a b))))
 
-(define (make-product a b) (list '* a b))
+(define (make-product a b)
+  (cond ((and (integer? a) (= a 0)) 0)
+	((and (integer? b) (= b 0)) 0)
+	((and (integer? a) (integer? b)) (* a b))
+	((and (integer? a) (= a 1)) b)
+	((and (integer? b) (= b 1)) a)
+	((eq? a b) (list '^ a 2))
+	(else (list '* a b))))
 
 (define a1 cadr)
 
