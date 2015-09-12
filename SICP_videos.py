b06096edfe09ad5b879387c919ebff5aec4ecaf3
Kaushik Kannan <kaushik1618@gmail.com>
2015-09-12 11:44:46 -0700

    Fixing Python script for video URLs not matching template

--- SICP_videos.py
+++ SICP_videos.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python
 
-import urllib
+import re, urllib
 
 url_template = "http://www.archive.org/download/MIT_Structure_of_Computer_Programs_1986/lec%s%s_512kb.mp4"
 
 videos = [ url_template % (index, sub_index) for index in range(1,11) for sub_index in ['a', 'b'] ]
 
 for video in videos:
+    if any(lecture in video for lecture in ['lec6b', 'lec10a']):
+        video = video.replace('lec', 'Lec')
     print "Downloading %s ..." % video.split('/')[-1]
     urllib.urlretrieve( video, video.split('/')[-1] )
